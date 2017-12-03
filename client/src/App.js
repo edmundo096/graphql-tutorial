2478189f9e5761c7950e632a85aaf69b7b60b7ba
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-12-03 02:35:44 -0600

    feat(App): Add Apollo logo and ChannelsList component

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ChannelsList = () =>
+  (<ul>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Apollo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChannelsList />
       </div>
     );
   }
