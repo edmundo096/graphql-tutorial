8e1b974708421e3b742433466ee2d2bcb8ad4786
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-13 23:45:33 -0600

    feat(App): Add Apollo and graphql to ChannelsList and App components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
+
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+} from 'react-apollo';
+
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () =>
-  (<ul>
-    <li>Channel 1</li>
-    <li>Channel 2</li>
-  </ul>);
+
+const client = new ApolloClient();
+
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsList = ({data: {loading, error, channels}}) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return <ul>
+    { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+  </ul>;
+};
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Apollo</h1>
-        </header>
-        <ChannelsList />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Apollo</h1>
+          </header>
+          <ChannelsList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
