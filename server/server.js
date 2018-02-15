88b4f91593cad169ea4b71ff144709b378a8ffa4
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-15 08:49:43 -0600

    feat(): Connect App to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import cors from 'cors';
 import {
   graphqlExpress,
   graphiqlExpress,
@@ -8,8 +9,11 @@ import bodyParser from 'body-parser';
 
 import { schema } from './src/schema';
 
+
 const PORT = 4000;
 const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:3000' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
