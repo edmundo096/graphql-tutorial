3c798fe2c5b62e69d8fdbec8675e6fd496d265be
Edmundo Elizondo <edmundo.geg@hotmail.com>
2018-02-15 10:16:33 -0600

    feat(client): Add channel 5 sec pulling

--- client/src/components/ChannelsListWithData.js
+++ client/src/components/ChannelsListWithData.js
@@ -32,4 +32,4 @@ export const channelsListQuery = gql`
   }
 `;
 
-export default graphql(channelsListQuery)(ChannelsList);
+export default graphql(channelsListQuery, { options: {pollInterval: 5000} })(ChannelsList);
